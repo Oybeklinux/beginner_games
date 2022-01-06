86a2087f984e18667876b3dc396a620d8a83e647
Oybeklinux <Oybeklinux@gmail.com>
2022-01-06 15:10:09 +0500

    game 3

---
+++ 3. Sonni top (Kompyuter topsin)/main.py
@@ -0,0 +1,16 @@
+import random
+
+boshi = 1
+ohiri = 100
+counter = 0
+for i in range(1_000_000):
+    son = random.randint(boshi, ohiri)
+    javob = input(f"{son} Son tengmi (t) Son kattami (k) Son kichikmi (c): ")
+    counter += 1
+    if javob == "t":
+        print(f"Topdim. Urinishlar soni: {counter}")
+        break
+    elif javob == "k":
+        ohiri = son - 1
+    elif javob == "c":
+        boshi = son + 1
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # beginner_games
+1. Guess number
+[https://www.youtube.com/watch?v=49qJKt6AdJs&list=PLZ67NWgKA8g54KTNynnEepd07LXQrUO47&index=2]
