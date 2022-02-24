311ace06bdce1a63e0304cd9e231e9455eb0cbde
Oybeklinux <Oybeklinux@gmail.com>
2022-02-24 16:17:39 +0500

    game 7

--- 7. Musobaqa/Vazifalar.md
+++ 7. Musobaqa/Vazifalar.md
@@ -10,40 +10,22 @@ ________ Learn English words ________
 - show_results()
 - register()
 - login()
-- sub_menu
 
 3. Menyuni cheksiz tsiklga oling. So'ng tanlovga mos funksiyalarni chaqiradigan qiling. Ya'ni agar foydalanuvchi 
 - 1 ni tanlasa show_results()
 - 2 ni tanlasa register()
 - 3 ni tanlasa login()
 - 4 ni tanlasa chiqib ketsin
-
-4. Natijalarni ko'rsatish (show_results) funksiyasini yozing. Bazani *users* deb nomlang:
-- Baza ko'rinishi quyidagicha bo'ladi:
-```text
-oybek,20
-otabek,10
-```
-- Agar baza mavjud bo'lsa va login bazada bo'lmasa, ma'lumot quyidagi jadval ko'rishinishida chiqsin
-```text
-O'yin natijalari:
-1        Otabek                 40         
-2        Oybek                  10
-```
-- Agar login bazada mavjud bo'lsa, qayta login kiritishni so'rasin:
-```text
-Login kiriting: oybek
-Bu login bazada bor. Boshqa login yozing: 
-Login kiriting:
-```
-- Agar baza mavjud bo'lmasa quyidagi habar chiqsin
-```text
-Natijalar mavjud emas
-```
-5. register() funksiyasini yozamiz:
+ 
+4. register() funksiyasini yozamiz:
+   1. Foydalanuvchidan login va parolni so'rab, uni json formatda faylga yozamiz
+   2. Agar fayl bo'lsa, ketidan yozsin
+   3. Agar faylda bunday login mavjud bo'lsa, qayta kiritsin
+   
 - Agar ro'yxatdan o'tmagan bo'lsa
 ```text
 Login kiriting: oybek
+Parolni kiriting: 123
 Hush kelibsiz oybek
 ```
 - Aks holda quyidagi habar chiqsin
@@ -51,11 +33,79 @@ Hush kelibsiz oybek
 Login kiriting: oybek
 Bu login bazada bor. Boshqa login yozing: 
 Login kiriting: otabek
+Parolni kiriting: 234
 Hush kelibsiz otabek
+Bittasini tanlang:
+        1. O'yinni boshlash
+        2. Natijalarni ko'rish
+        3. Orqaga qaytish
 ```
-6. login() funksiyasi.
+5. login() funksiyasi.
+   1. Foydalanuvchidan login so'rasin
+   2. Fayldan hamma foydalanuvchilar ro'yxatini o'qib loginni solishtirsin, agar login bazada mavjud bo'lmasa, chiqib ketsin
+   3. Agar login bazada bo'lsa, so'ng parolni so'rasin, parolni ham solishtirsin, agar teng bo'lsa, keyingi menyuni ochsin
+     
+- Agar ro'yxatdan o'tgan bo'lmasa
+```text
+Kirish uchun loginni kiriting: test
+test logini bazada mavjud emas. Avval ro'yxatdan o'ting
+________ Learn English words ________ 
+    1. O'yin natijalari
+    2. Ro'yxatdan o'tish
+    3. Kirish
+    4. Chiqish
+```
 - Agar ro'yxatdan o'tgan bo'lsa
 ```text
-Kirish uchun loginni kiriting: ulugbek
-ulugbek logini bazada mavjud emas. Avval ro'yxatdan o'ting
+Kirish uchun loginni kiriting: oybek
+parol: 123
+Hush kelibsiz oybek
+Bittasini tanlang:
+        1. O'yinni boshlash
+        2. Natijalarni ko'rish
+        3. Orqaga qaytish
+```
+6. play() funksiyasini yozish:
+- words.txt degan fayl yarating. So'ng quyidagi ko'rinishida so'zlarni saqlang:
+```text
+maktab,school
+kuchuk,dog
+mushuk,cat
+```
+- so'zlarni fayldan o'qib o'zbekcha inglizcha so'zlar ro'yxatni hosil qiling
+- cheksiz tsiklda tasodifiy so'z tanlasin ekranga chiqarsin
+- chiqarilgan so'z boshqa qaytarilmasin
+- foydalnuvchidan tarjimasi so'ralsin, so'ng taqqoslasin, xato va to'g'ri javoblar hisoblansin
+- 3 xatodan so'ng natijani e'lon qilib chiqib ketsin
+- foydalanuvchi natijasi oldingi natijadan katta bo'lsa, yangilab qo'ysin
+  - hamma foydalanuvchilarni fayldan o'qib olamiz
+  - tsiklda foydalanuvchi logini yordamida usha foydalanuvchini topamiz
+  - bali katta bo'lsa, o'zgartirib, yana yozish uchun fayl ochib yozib qo'yamiz
+```text
+yugurmoq so'zni tarjimasini yozing: runn
+```
+- foydalanuvchi kiritgan so'z to'g'ri bo'lsa, keyingi so'z chiqadi, agar xato bo'lsa, quyidagi habar chiqadi
+```text
+Xato. To'g'ri javob: run
+```
+- natijasni users faylida saqlab qo'ying
+- xato qilsa, o'yin tugaydi, xato va to'g'ri javoblar ko'rsatiladi:
+```text
+Xatolar soni: 3
+To'g'ri javoblar soni: 0
+Bittasini tanlang:
+        1. O'yinni boshlash
+        2. Natijalarni ko'rish
+        3. Orqaga qaytish
+```
+7. Natijalarni ko'rsatish (show_results) funksiyasini yozing. Bazani *users* deb nomlang:
+- Agar foydalanuvchilar bazasi mavjud bo'lsa, ma'lumot quyidagi kamayish tartibida chiqsin
+```text
+O'yin natijalari:
+1        Otabek                 40         
+2        Oybek                  10
+```
+- Agar baza mavjud bo'lmasa quyidagi habar chiqsin
+```text
+Natijalar mavjud emas
 ```
--- 7. Musobaqa/main.py
+++ 7. Musobaqa/main.py
@@ -1,158 +1,150 @@
+import json
 import os
-import random
+from random import choice
 
-file_users = "users"
-file_words = "words"
+user_file = "users.txt"
+word_file = "words.txt"
+
+
+def sort_by_point(user):
+    return user['point']
+
+
+def show_results():
+    if not os.path.exists(user_file):
+        print("Natijalar mavjud emas")
+        return
+
+    with open(user_file) as file:
+        users:list = json.load(file)
+    users.sort(key=sort_by_point, reverse=True)
+    print("O'yin natijalari:")
+    index = 0
+    for user in users:
+        index += 1
+        print(f"{index}{user['login'].center(20)}{str(user['point']).center(20)}")
+
+
+def play(login):
+    with open(word_file, 'r') as file:
+        words = file.readlines()
+    asked_words = []
+    wrong_answer = 0
+    right_answer = 0
+    while True:
+        while True:
+            word = choice(words)
+            if word not in asked_words:
+                break
+        asked_words.append(word)
+        uz, en = word.split(",")
+        en = en.strip()
+        user_en = input(f"{uz} so'zni tarjimasini yozing: ")
+        if user_en == en:
+            right_answer += 1
+        else:
+            wrong_answer += 1
+            print(f"Xato. To'g'ri javob: {en}")
+            if wrong_answer == 3:
+                print(f"Xatolar soni: {wrong_answer}\nTo'g'ri javoblar soni: {right_answer}")
+                break
+    with open(user_file) as file:
+        users = json.load(file)
+    for i in range(len(users)):
+        if users[i]['login'] == login:
+            point = right_answer * 10
+            if users[i]['point'] < point:
+                users[i]['point'] = point
+                with open(user_file, 'w') as file:
+                    json.dump(users, file, indent=4)
+
+
+def sub_menu(login):
+    while True:
+        habar = """Bittasini tanlang:
+            1. O'yinni boshlash
+            2. Natijalarni ko'rish
+            3. Orqaga qaytish\n"""
+        tanlov = menu(habar, range(1,4))
+        if tanlov == 1:
+            play(login)
+        elif tanlov == 2:
+            show_results()
+        else:
+            break
 
 
 def register():
-    file = None
-    while True:
-        login = input("Login kiriting: ")
-        if os.path.exists(file_users):
-            file = open(file_users)
+    if not os.path.exists(user_file):
+        user = {
+            'login': input("Login kiriting: "),
+            'password': input("Parolni kiriting: "),
+            'point': 0
+        }
+        users = [user]
+        with open(user_file, 'w') as file:
+            json.dump(users, file, indent=4)
+    else:
+        with open(user_file) as file:
+            users = json.load(file)
+        while True:
+            user = {
+                'login': input("Login kiriting: "),
+                'point': 0
+            }
             user_exist = False
-            for user in file.readlines():
-                if login.lower() == user.strip():
+            for user_ in users:
+                if user_['login'] == user['login']:
                     print("Bu login bazada bor. Boshqa login yozing: ")
                     user_exist = True
                     break
             if not user_exist:
+                user['password'] = input("Parolni kiriting: ")
+                with open(user_file, 'w') as file:
+                    json.dump(users, file, indent=4)
                 break
-        else:
-            break
-    if file:
-        file.close()
-    file = open(file_users, 'a')
-    file.write(f"{login}\n")
-    return login
+    sub_menu(user['login'])
 
 
 def login():
-    if not os.path.exists(file_users):
-        return None, "Hozircha hech kim ro'yxatdan o'tmadi. Avval ro'yxatdan o'ting"
-    user_login = input("Kirish uchun loginni kiriting: ")
-    file = open(file_users)
-    for db_login in file.readlines():
-        values = db_login.split(",")
-        if user_login == values[0].strip():
-            return user_login, None
+    login = input("Kirish uchun loginni kiriting: ")
+    with open(user_file) as file:
+        users = json.load(file)
+    for user in users:
+        if user['login'].lower() == login.lower():
+            parol = input("Parol: ")
+            if user['password'] == parol:
+                sub_menu(login)
+                return
+            else:
+                print("Parol xato yozildi")
+                return
+    print(f"{login} logini bazada mavjud emas. Avval ro'yxatdan o'ting")
 
-    return None, f"{user_login} logini bazada mavjud emas. Avval ro'yxatdan o'ting"
 
 
-def menu(habar: str, lst: list):
-    birinchi = True
+
+def menu(habar, royxat):
     while True:
-        if birinchi:
-            tanlov = input(habar)
-            birinchi = False
-        else:
-            tanlov = input("Xato. Qayta tering: ")
-        if tanlov.isdigit() and int(tanlov) in lst:
+        tanlov = input(habar)
+        if tanlov.isdigit() and int(tanlov) in royxat:
             return int(tanlov)
 
 
-def sub_menu(user_login):
-    habar = f"Hush kelibsiz {user_login}"
-    while True:
-        os.system("cls")
-        print(habar)
-        tanlov = menu("""Bittasini tanlang:
-        1. O'yinni boshlash
-        2. Natijalarni ko'rish
-        3. Orqaga qaytish\n""", range(1, 4))
-        if tanlov == 1:
-            habar = play(user_login)
-        elif tanlov == 2:
-            habar = show_results()
-        elif tanlov == 3:
-            break
-
-
-def show_results():
-    if not os.path.exists(file_users):
-        return "Natijalar mavjud emas"
-    file = open(file_users)
-    users = {}
-    habar = "O'yin natijalari:\n"
-    for user in file.readlines():
-        values = user.split(",")
-        if len(values) == 2:
-            users[values[1].strip()] = values[0].strip()
-        else:
-            users[values[0].strip()] = 0
-
-    index = 0
-    for value, key in sorted(users.items(), reverse=True):
-        index += 1
-        habar += f"{index} {str(key).center(20)} {value.center(20)}\n"
-    return habar
-
-
-def play(user):
-    file = open(file_words, 'rt')
-    lines = file.readlines()
-    file.close()
-    length = len(lines)
-    user_points = 0
-    wrong_answers = 0
-    max_wrong_answers = 3
-    habar = ""
-    while True:
-        line = random.randint(1, length)
-        glossary = lines[line-1].split(",")
-        uz, en = glossary[1], glossary[0]
-        answer = input(f"{uz.strip()} so'zni tarjimasini yozing: ")
-        if answer == en:
-            user_points += 10
-        else:
-            print(f"Xato. To'g'ri javob: {en}")
-            wrong_answers += 1
-        if wrong_answers == 3:
-            habar = f"Xatolar soni: {max_wrong_answers}\nTo'g'ri javoblar soni: {user_points//10}"
-            break
-    return write_results(user_points, user, habar)
-
-
-def write_results(point, user_login, habar):
-    file = open(file_users, 'r')
-    users = file.readlines()
-    for i in range(len(users)):
-        values = users[i].split(",")
-        if values[0].strip() == user_login:
-            if len(values) == 2 and int(values[1].strip()) < point:
-                users[i] = f"{values[0].strip()},{point}\n"
-                habar += "\nAvvalgi natijalaringizga nisbatan rekord o'rnatdingiz"
-            elif len(values) == 1:
-                users[i] = f"{values[0].strip()},{point}\n"
-            break
-
-    file.close()
-    file = open(file_users, 'w')
-    file.write(''.join(users))
-    file.close()
-    return habar
-
-
 if __name__ == "__main__":
-    habar = ""
     while True:
-        os.system("cls")
-        print(habar)
-        tanlov = menu("""________ Learn English words ________ 
-    1. O'yin natijalari
-    2. Ro'yxatdan o'tish
-    3. Kirish
-    4. Chiqish\n""", range(1, 5))
+        habar = """________ Learn English words ________ 
+            1. O'yin natijalari
+            2. Ro'yxatdan o'tish
+            3. Kirish
+            4. Chiqish\n"""
+        tanlov = menu(habar, range(1, 5))
+        print(tanlov)
         if tanlov == 1:
-            habar = show_results()
+            show_results()
         elif tanlov == 2:
-            user_login = register()
-            sub_menu(user_login)
+            register()
         elif tanlov == 3:
-            user_login, habar = login()
-            if user_login:
-                sub_menu(user_login)
-        elif tanlov == 4:
+            login()
+        else:
             break
